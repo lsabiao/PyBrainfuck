a07ef49b71c669bca220eff2cb1b29648a131cb9
Lucas Sabião <lsabiao@users.noreply.github.com>
2015-03-24 22:42:22 -0300

    Update PyBrainfuck.py with PEP8

--- PyBrainfuck.py
+++ PyBrainfuck.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#interpretador python de Brainfuck
+# interpretador python de Brainfuck
 
-#Implementação da especificação da linguagem
+# Implementação da especificação da linguagem
 
-#Os 8 comandos
+# Os 8 comandos
 
-#comandos = {"+":"mais",          incrementa em 1 o valor da célula atual
+# comandos = {"+":"mais",          incrementa em 1 o valor da célula atual
 #            "-":"menos",         decrementa em 1 o valor da célula atual
 #            ">":"sobePonteiro",  incrementa em 1 a localização da célula atual
 #            "<":"descePonteiro", decrementa em 1 a localização da célula atual
@@ -22,7 +22,7 @@
 #                                 o "]" correspondente, se não faz a próxima
 #                                 instrução
 #
-#            "]":"loopEnd"        se o valor  na célula atual for zero, vai para
+#            "]":"loopEnd"        se o valor na célula atual for zero, vai para
 #                                 a próxima instrução, se não volta para o "["
 #                                 correspondente
 #           }
@@ -33,13 +33,14 @@ import sys
 __version__ = "1.0"
 __author__ = "lucas.sabiao@hotmail.com"
 
-estrutura = [0] * 30000 #um vetor com 30.000 posições, isso deve ser o programa
-ponteiro = 0            #um ponteiro indicando qual é a célula atual do programa
-programa = ""           #o programa deve estar contido nessa string
-posicao = 0             #qual instrução está sendo interpretada
+estrutura = [0] * 30000  # um vetor com 30000 posições,isso deve ser o programa
+ponteiro = 0             # um ponteiro indicando qual é a célula atual
+programa = ""            # o programa deve estar contido nessa string
+posicao = 0              # qual instrução está sendo interpretada
+
 
 def checkLoop():
-    #procura por uma quantidade par de '[' e ']'
+    # procura por uma quantidade par de '[' e ']'
     global programa
     aberto = programa.count("[")
     fechado = programa.count("]")
@@ -49,35 +50,42 @@ def checkLoop():
         print ("Quantidade estranha de '[]'")
         return False
 
-#implementação de cada instrução como uma funcão
+# implementação de cada instrução como uma funcão
 
-#não fiquei feliz com essa quantidade absurda de "global"s, mas não achei
-#nenhuma saída bem didática para isso.
+# não fiquei feliz com essa quantidade absurda de "global"s, mas não achei
+# nenhuma saída bem didática para isso.
+
+
 def mais():
     global estrutura
     global ponteiro
-    estrutura[ponteiro]+=1
+    estrutura[ponteiro] += 1
+
 
 def menos():
     global estrutura
     global ponteiro
-    estrutura[ponteiro]-=1
+    estrutura[ponteiro] -= 1
+
 
 def sobePonteiro():
     global estrutura
     global ponteiro
-    ponteiro+=1
+    ponteiro += 1
+
 
 def descePonteiro():
     global estrutura
     global ponteiro
-    ponteiro-=1
+    ponteiro -= 1
+
 
 def printASCII():
     global estrutura
     global ponteiro
     print chr(estrutura[ponteiro]),
     sys.stdout.flush()
+
 
 def recebeASCII():
     global estrutura
@@ -89,9 +97,11 @@ def recebeASCII():
         estrutura[ponteiro] = 0
 
 
-#Fiquei muito em dúvida sobre como fazer os loops.
-#uma abordagem de manter na memória os endereços de cada "abrir" e "fechar"
-#seria muito mais performática e limpo, mas achei que ficaria mais ilegível.
+# Fiquei muito em dúvida sobre como fazer os loops.
+# uma abordagem de manter na memória os endereços de cada "abrir" e "fechar"
+# seria muito mais performática e limpo, mas achei que ficaria mais ilegível.
+
+
 def loop():
     global estrutura
     global ponteiro
@@ -101,6 +111,7 @@ def loop():
     else:
         pass
 
+
 def loopEnd():
     global estrutura
     global ponteiro
@@ -108,7 +119,8 @@ def loopEnd():
     if(estrutura[ponteiro] == 0):
         pass
     else:
-        posicao = procurarLoop()-1
+        posicao = procurarLoop() - 1
+
 
 def procurarEnd():
     global programa
@@ -119,28 +131,30 @@ def procurarEnd():
         if(aux == 0):
             return posicaoAtual
         if(instrucao == "["):
-            aux+=1
+            aux += 1
         elif(instrucao == "]"):
-            aux-=1
-        posicaoAtual+=1
+            aux -= 1
+        posicaoAtual += 1
+
 
 def procurarLoop():
     global programa
     global posicao
     aux = 1
     posicaoAtual = posicao
-    for p in range(len(programa[:posicao])-1,0,-1):
+    for p in range(len(programa[:posicao])-1, 0, -1):
         if(programa[p] == "]"):
-            aux+=1
+            aux += 1
         elif(programa[p] == "["):
-            aux-=1
+            aux -= 1
         if(aux == 0):
             return posicaoAtual
-        posicaoAtual-=1
+        posicaoAtual -= 1
+
 
 def validarCelula():
-    #python não suporta variáveis como byte.
-    #workaround
+    # python não suporta variáveis como byte.
+    # workaround
 
     global estrutura
     global ponteiro
@@ -148,6 +162,7 @@ def validarCelula():
         estrutura[ponteiro] = 255
     elif (estrutura[ponteiro] < 0):
         estrutura[ponteiro] = 0
+
 
 def runPrograma():
     global programa
@@ -158,7 +173,7 @@ def runPrograma():
         while True:
             validarCelula()
             try:
-                #não é a saida mais elegante, mas funciona muito bem
+                # não é a saida mais elegante, mas funciona muito bem
                 instrucao = programa[posicao]
             except IndexError:
                 break
@@ -178,29 +193,30 @@ def runPrograma():
                 loop()
             elif (instrucao == "]"):
                 loopEnd()
-            posicao+=1
+            posicao += 1
 
 if __name__ == "__main__":
 
-    #Uso:
-    #sem argumentos: pede para você digitar o programa
+    # Uso:
+    # sem argumentos: pede para você digitar o programa
 
-    #exemplo ou exemplo.bfmostrar na tela o programa "Hello World"
-    #baseado no artigo en.wikipedia.org/wiki/Brainfuck
+    # exemplo ou exemplo.bfmostrar na tela o programa "Hello World"
+    # baseado no artigo en.wikipedia.org/wiki/Brainfuck
 
-    #como interpretador, recebe o caminho para o arquivo a ser interpretado
+    # como interpretador, recebe o caminho para o arquivo a ser interpretado
 
     from sys import argv
     from os import getcwd
-    if(len(argv)>1):
-        if (argv[1].lower() == "exemplo" or argv[1].lower() == "exemplo.bf"): #hello world
+    if(len(argv) > 1):
+        if (argv[1].lower() == "exemplo" or argv[1].lower() == "exemplo.bf"):
+            # hello world
             programa = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>   \
-                        .>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+                    .>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
         else:
-            #pegar o arquivo para interpretar
+            # pegar o arquivo para interpretar
             path = getcwd()+"/"+argv[1]
             try:
-                programa = open(path,"r").read()
+                programa = open(path, "r").read()
             except:
                 print "Progama não encontrado"
     else:
